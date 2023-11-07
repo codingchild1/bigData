70e0a1ef7d6a7a2cb8e032b76232b3d7b7257dc5
codingchild1 <tkddnjs04230@naver.com>
2023-11-07 10:46:01 +0900

    20231107 jython

--- src/main/java/com/example/crawling/main/MongoinsertApplication.java
+++ src/main/java/com/example/crawling/main/MongoinsertApplication.java
@@ -1,10 +1,9 @@
 package com.example.crawling.main;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.python.core.PyFunction;
+import org.python.core.PyInteger;
+import org.python.core.PyObject;
+import org.python.util.PythonInterpreter;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,6 +20,8 @@ public class MongoinsertApplication {
     @Resource
     private BookRepository bookRepository;
 
+    private PythonInterpreter interpreter;
+
     public static void main(String[] args) {
         SpringApplication.run(MongoinsertApplication.class, args);
     }
@@ -30,53 +31,53 @@ public class MongoinsertApplication {
         return args -> {
 
 //            bookRepository.deleteAll();   //데이터 삭제
-
-            //뉴스번호 7753100번부터 7753110번까지 스크래핑
-            for (int newsNo = 7803750; newsNo <= 7803760; newsNo++) {
-
-                final String url = "https://news.kbs.co.kr/news/pc/view/view.do?ncd="+newsNo;
-
-                try {
-                    Connection conn = Jsoup.connect(url);
-                    Document document = conn.get();
-
-                    //뉴스 동영상 URL
-//                Element imageUrl = document.getElementsByAttributeValue("alt", "동영상 링크").first();
-
-                    //기사 제목
-                    Element title = document.getElementsByClass("headline-title").first();
-
-                    //보도 날짜
-                    Element date = document.getElementsByClass("input-date").first();
-
-                    //본문
-                    Elements detail = document.getElementsByClass("detail-body");
-                    String strDetail = detail.html();
-                    strDetail = strDetail.replaceAll("<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>", " ");
-                    strDetail = strDetail.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9/\\s/g]", "");
-                    strDetail = strDetail.replaceAll("서비스 기사   기사본문 필요한 부분을 치환  ", "");
-
-                    //기자 이름
-                    Elements reporter = document.getElementsByClass("reporter-name");
-
-                    //기자 이메일
-                    Elements email = document.getElementsByClass("ico-email");
-
-                /* html() : 태그까지 가져오기
-                   text() : 내용만 가져오기 */
-
-                    System.out.println("newsNo : " + newsNo);
-//                System.out.println("상품 이미지 URL : " + imageUrl.attr("abs:src"));
-                    System.out.println("기사 제목 : " + title.text());
-                    System.out.println("기사 날짜 : " + date.text());
-                    System.out.println("본문 : " + strDetail);
-                    System.out.println("기자 : " + reporter.text());
-                    System.out.println("이메일 : " + email.text());
-//                for (int infoIdx = 0; infoIdx < info.size(); infoIdx++) {
-//                    System.out.println(productInfo[infoIdx] + info.get(infoIdx).text());
-//                }
-                    System.out.println("---------------------------");
-
+//
+//            //뉴스번호 7753100번부터 7753110번까지 스크래핑
+//            for (int newsNo = 7803750; newsNo <= 7803760; newsNo++) {
+//
+//                final String url = "https://news.kbs.co.kr/news/pc/view/view.do?ncd="+newsNo;
+//
+//                try {
+//                    Connection conn = Jsoup.connect(url);
+//                    Document document = conn.get();
+//
+//                    //뉴스 동영상 URL
+////                Element imageUrl = document.getElementsByAttributeValue("alt", "동영상 링크").first();
+//
+//                    //기사 제목
+//                    Element title = document.getElementsByClass("headline-title").first();
+//
+//                    //보도 날짜
+//                    Element date = document.getElementsByClass("input-date").first();
+//
+//                    //본문
+//                    Elements detail = document.getElementsByClass("detail-body");
+//                    String strDetail = detail.html();
+//                    strDetail = strDetail.replaceAll("<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>", " ");
+//                    strDetail = strDetail.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9/\\s/g]", "");
+//                    strDetail = strDetail.replaceAll("서비스 기사   기사본문 필요한 부분을 치환  ", "");
+//
+//                    //기자 이름
+//                    Elements reporter = document.getElementsByClass("reporter-name");
+//
+//                    //기자 이메일
+//                    Elements email = document.getElementsByClass("ico-email");
+//
+//                /* html() : 태그까지 가져오기
+//                   text() : 내용만 가져오기 */
+//
+//                    System.out.println("newsNo : " + newsNo);
+////                System.out.println("상품 이미지 URL : " + imageUrl.attr("abs:src"));
+//                    System.out.println("기사 제목 : " + title.text());
+//                    System.out.println("기사 날짜 : " + date.text());
+//                    System.out.println("본문 : " + strDetail);
+//                    System.out.println("기자 : " + reporter.text());
+//                    System.out.println("이메일 : " + email.text());
+////                for (int infoIdx = 0; infoIdx < info.size(); infoIdx++) {
+////                    System.out.println(productInfo[infoIdx] + info.get(infoIdx).text());
+////                }
+//                    System.out.println("---------------------------");
+//
 //                    Book book = new Book();
 //                    book.setNewsNo(newsNo);
 //                    book.setTitle(title.text());
@@ -86,15 +87,31 @@ public class MongoinsertApplication {
 //                    book.setEmail(email.text());
 //
 //                    bookRepository.insert(book);
+//
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            for (Book book: bookRepository.findAll()) {
+//                System.out.println(book.getTitle().toString());
+//            }
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+            System.out.println("파이썬 코드");
+            System.setProperty("python.import.site", "false");
 
-            for (Book book: bookRepository.findAll()) {
-                System.out.println(book.toString());
-            }
+            interpreter = new PythonInterpreter();
+            interpreter.execfile("C:/Users/ncloud/IdeaProjects/crawling/src/main/resources/jython/test.py");
+            interpreter.exec("print(testFunc(5, 10)");
+
+            PyFunction pyFunction = interpreter.get("testFunc", PyFunction.class);
+
+            int a = 10;
+            int b = 20;
+
+            PyObject pyobj = pyFunction.__call__(new PyInteger(a), new PyInteger(b));
+            System.out.println(pyobj.toString());
+
         };
     }
 }
