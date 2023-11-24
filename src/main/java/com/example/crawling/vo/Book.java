48941b45eb2d2082d3f33f31743a8f2d275f19f9
codingchild1 <tkddnjs04230@naver.com>
2023-11-24 14:48:27 +0900

    db 서버 변경 db 구조변경 morph 특수문자 제거

--- src/main/java/com/example/crawling/vo/Book.java
+++ src/main/java/com/example/crawling/vo/Book.java
@@ -6,18 +6,28 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data   /*getter setter toString*/
 @NoArgsConstructor  /*기본 생성자를 유지하고 인자를 받는 생성자*/
 @AllArgsConstructor /*모든 필드를 가지고 있는 생성자를 자동으로 생성*/
-@Document(collection = "test")  /*MongoDB의 test 컬렉션과 연결하도록 설정*/
+@Document(collection = "crawling")  /*MongoDB의 test 컬렉션과 연결하도록 설정*/
 public class Book {
     @Id
     private int newsNo;
+    private String url;    // URL 타입?
     private String title;
     private String date;
     private String detail;
     private String reporter;
     private String email;
+    private String nontagDetail;
+    private String nontagTitle;
+    private String analyzeDetail;
+    private String analyzeTitle;
+    private List<?> nounDetail;
+    private List<?> nounTitle;
+    private String crawlDate;
 
 //    public Book() {}
 
